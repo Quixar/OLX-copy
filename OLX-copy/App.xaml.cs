a8da1eca24966bb72780a9f1f063649ad4cb3611
Quixar <evgenmart01@gmail.com>
2025-05-13 21:19:02 +0300

    Добавьте файлы проекта.

---
+++ OLX-copy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OLX_copy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
