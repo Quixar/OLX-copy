cd5ac525d1c2e60705f1f636923a3b546679b006
Quixar <evgenmart01@gmail.com>
2025-05-26 17:44:11 +0300

    MVVM pattern for LoginWindow

---
+++ OLX-copy/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using OLX_copy.Data;
+using OLX_copy.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace OLX_copy.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(nameof(Username)); OnPropertyChanged(nameof(IsUsernameEmpty)); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(nameof(Password)); OnPropertyChanged(nameof(IsPasswordEmpty)); }
+        }
+
+        public bool IsUsernameEmpty => string.IsNullOrEmpty(Username);
+        public bool IsPasswordEmpty => string.IsNullOrEmpty(Password);
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        private void Login(object obj)
+        {
+            try
+            {
+                using var context = new DataContext();
+                var userAccess = context.UserAccesses
+                    .Include(u => u.User)
+                    .FirstOrDefault(u => u.Login == Username);
+
+                if (userAccess == null || !PasswordHasher.VerifyPassword(Password, userAccess.Salt, userAccess.Dk))
+                {
+                    MessageBox.Show("Invalid username or password.");
+                    return;
+                }
+
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                var loginWindow = Application.Current.Windows
+                    .OfType<Window>()
+                    .FirstOrDefault(w => w is OLX_copy.LoginWindow);
+
+                loginWindow?.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Login failed: " + ex.Message);
+            }
+        }
+
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ OLX-copy/Views/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using OLX_copy.ViewModels;
+
+namespace OLX_copy
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox && DataContext is LoginViewModel vm)
+            {
+                vm.Password = passwordBox.Password;
+            }
+
+            if (FindName("PasswordWatermark") is TextBlock passwordWatermark)
+            {
+                passwordWatermark.Visibility = string.IsNullOrEmpty(((PasswordBox)sender).Password)
+                    ? Visibility.Visible
+                    : Visibility.Collapsed;
+            }
+        }
+
+        private void RegisterTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+            this.Close();
+        }
+    }
+}
