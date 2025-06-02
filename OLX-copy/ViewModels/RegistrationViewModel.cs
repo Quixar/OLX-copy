31d2dbd624e3470fc1c3e4762b4dfc6e71b0a992
Quixar <evgenmart01@gmail.com>
2025-06-02 20:16:22 +0300

    MVVM pattern for RegistrationWindow

---
+++ OLX-copy/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,155 @@
+﻿using OLX_copy.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
+
+namespace OLX_copy.ViewModels
+{
+    public class RegistrationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _username;
+        private string _password;
+        private string _email;
+        private string _name;
+
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(nameof(Username)); OnPropertyChanged(nameof(IsUsernameEmpty)); }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(nameof(Email)); OnPropertyChanged(nameof(IsEmailEmpty)); }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(nameof(Name)); OnPropertyChanged(nameof(IsNameEmpty)); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(nameof(Password)); OnPropertyChanged(nameof(IsPasswordEmpty)); }
+        }
+
+
+        public bool IsUsernameEmpty => string.IsNullOrEmpty(Username);
+        public bool IsPasswordEmpty => string.IsNullOrEmpty(Password);
+        public bool IsEmailEmpty => string.IsNullOrEmpty(Email);
+        public bool IsNameEmpty => string.IsNullOrEmpty(Name);
+
+        public ICommand RegisterCommand { get; }
+
+        public RegistrationViewModel()
+        {
+            RegisterCommand = new RelayCommand(Register);
+        }
+
+        private void Register(object obj)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password) || string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Name))
+                {
+                    MessageBox.Show("Please fill in all fields.");
+                    return;
+                }
+
+                if (!IsValidEmail(Email))
+                {
+                    MessageBox.Show("Invalid email format.");
+                    return;
+                }
+
+                if (Password.Length < 6)
+                {
+                    MessageBox.Show("Password must be at least 6 characters long.");
+                    return;
+                }
+
+                using var context = new Data.DataContext();
+
+                if (context.UserAccesses.Any(u => u.Login == Username))
+                {
+                    MessageBox.Show("This login is already taken.");
+                    return;
+                }
+
+                Guid userId = Guid.NewGuid();
+                string salt = PasswordHasher.GenerateSalt();
+                string dk = PasswordHasher.HashPassword(Password, salt);
+
+                var user = new Data.Entities.User
+                {
+                    Id = userId,
+                    Name = Name,
+                    Email = Email,
+                    RegisteredAt = DateTime.Now
+                };
+
+                var userAccess = new Data.Entities.UserAccess
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    RoleId = "user",
+                    Login = Username,
+                    Salt = salt,
+                    Dk = dk
+                };
+
+                context.Users.Add(user);
+                context.UserAccesses.Add(userAccess);
+                context.SaveChanges();
+
+                MessageBox.Show("Registration successful!");
+
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                var loginWindow = Application.Current.Windows
+                    .OfType<Window>()
+                    .FirstOrDefault(w => w is OLX_copy.RegistrationWindow);
+
+                loginWindow?.Close();
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.Message;
+
+                if (ex.InnerException != null)
+                    errorMessage += "\nInner Exception: " + ex.InnerException.Message;
+
+                if (ex.InnerException?.InnerException != null)
+                    errorMessage += "\nDeepest: " + ex.InnerException.InnerException.Message;
+
+                MessageBox.Show($"Error on registration: {errorMessage}");
+            }
+        }
+
+
+        private bool IsValidEmail(string email)
+        {
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
---
+++ OLX-copy/Views/RegistrationWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OLX_copy.ViewModels;
+
+namespace OLX_copy
+{
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+
+            this.Close();
+        }
+
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox && DataContext is RegistrationViewModel vm)
+            {
+                vm.Password = passwordBox.Password;
+            }
+
+            if (FindName("PasswordWatermark") is TextBlock passwordWatermark)
+            {
+                passwordWatermark.Visibility = string.IsNullOrEmpty(((PasswordBox)sender).Password)
+                    ? Visibility.Visible
+                    : Visibility.Collapsed;
+            }
+        }
+    }
+}
